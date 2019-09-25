2ad1c9bf6de75206b219b6b8f75c329963a2c3c0
Matt. Toczek <toczek.matt@gmail.com>
2019-09-25 10:38:23 +0100

    adding extra fetch and 2*functions to generate images and dropdown options from API

--- js/app.js
+++ js/app.js
@@ -7,14 +7,30 @@ const form = document.querySelector('form');
 // ------------------------------------------
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(data => console.log(data.message))
+    .then(data => generateImage(data.message))
 
+fetch('https://dog.ceo/api/breeds/list')
+    .then(response => response.json())
+    .then(data => generateOptions(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function generateImage(data) {
+    const html = `
+    <img src='${data}' alt></img>
+    <p>Click to view images of ${select.value}s</p>
+    `;
+    card.innerHTML = html;
+}
 
-
+function generateOptions(data){
+    const breedList = data.map(item => `
+        <option value='${item}'>${item}</option>
+    `);
+    
+    select.innerHTML = breedList;
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
